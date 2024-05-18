75b07ad7bc638e75fdf1ba5fffa0fb58d2d39a69
essaid bn <essaidcit00@gmail.com>
2024-05-18 18:15:59 +0100

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+import { defineNuxtConfig } from 'nuxt/config'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss'],
+  css: ['@/assets/scss/main.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/scss/variables.scss" as *;',
+        },
+      },
+    },
+  },
+})
