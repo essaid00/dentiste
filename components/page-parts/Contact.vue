8c98fc0bf8b1747c7809e67d1aa6fa07e3fb1dec
essaid bn <essaidcit00@gmail.com>
2024-05-20 14:04:27 +0100

    final commit

--- components/page-parts/Contact.vue
+++ components/page-parts/Contact.vue
@@ -43,14 +43,14 @@
       v-model="name.value.value"
       :counter="10"
       :error-messages="name.errorMessage.value"
-      label="Name"
+      label="Nom et Prénom"
     ></v-text-field>
 
     <v-text-field
       v-model="phone.value.value"
       :counter="7"
       :error-messages="phone.errorMessage.value"
-      label="Phone Number"
+      label="Numéro de téléphone"
     ></v-text-field>
 
     <v-text-field
@@ -63,16 +63,10 @@
       v-model="select.value.value"
       :error-messages="select.errorMessage.value"
       :items="items"
-      label="Select"
+      label="choisir"
     ></v-select>
 
-    <v-checkbox
-      v-model="checkbox.value.value"
-      :error-messages="checkbox.errorMessage.value"
-      label="Option"
-      type="checkbox"
-      value="1"
-    ></v-checkbox>
+
 
     <v-btn
       class="me-4 white--text bg-slate-500 bg-slate-300"
@@ -139,10 +133,10 @@
   const checkbox = useField('checkbox')
 
   const items = ref([
-    'Item 1',
-    'Item 2',
-    'Item 3',
-    'Item 4',
+    'proposition',
+    'message perso',
+    'lettre',
+    'sugjestion',
   ])
 
   const submit = handleSubmit(values => {
